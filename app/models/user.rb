f42456ce510d4d574235607f50c0a8e4ae222872
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-05 15:21:25 +0100

    Minor update on the validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   
-  validate_uniqueness_of :username, :email
+  validates_uniqueness_of :username, :email
 
-  validates :username presence: true, length: {minimum: 3}
+  validates :username, presence: true, length: {minimum: 3}
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/, message: 'must be a valid e-mail address' }
 end
